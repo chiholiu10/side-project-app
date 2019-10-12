b8bc1c42979974455a960cc26d85d52e9e1603cd
Chiho Liu <>
2019-10-12 20:22:17 +0200

    new update

--- src/components/DifferencePageOne.js
+++ src/components/DifferencePageOne.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const DifferencePageOne = () => {
+    const canvasRef = 'assets/find-differences/d1.svg';
+
+
     return (
         <div>
             DifferencePageOne
-        </div>
+            <img src={canvasRef}/>
+        </div>  
     )
 }
 
+// const useCanvas = (draw, context = "2d") => {
+//     const canvasRef = useRef(null);
+
+//     useEffect(() => {
+//         const ctx = canvasRef.current.getContext(context);
+//         let animationFrameId = requestAnimationFrame(renderFrame);
+    
+//         function renderFrame() {
+//           animationFrameId = requestAnimationFrame(renderFrame);
+//           draw(ctx);
+//         }
+    
+//         return () => cancelAnimationFrame(animationFrameId);
+//     }, []);
+//     return canvasRef;
+// }
 export default DifferencePageOne;
